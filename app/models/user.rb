422bba68b10b5822ee480cbcd8f12006ffc19959
Drew Sing <sing.drew@gmail.com>
2013-07-06 21:05:52 -0400

    sort of finished, now done with step 15. Need to verify all features work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
 	has_many :reviews
   has_many :products, :through => :reviews
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :name
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates_presence_of :name
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130705233203) do
+ActiveRecord::Schema.define(:version => 20130707005423) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20130705233203) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
